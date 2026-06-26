3d23b1730c02b9d633e50ac9e443abf4c9b17130
MohFahmiMc <mohf57880@gmail.com>
2026-06-26 22:51:27 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from 'next/navigation';
 
 export default function RootPage() {
   // Mengarahkan pengguna yang mengakses '/' langsung ke '/main'
-  redirect('/main');
+  redirect('freedom/main');
 }
